c427dbf8bb5bef8e1dcafa5f23b49c7f33e684a5
hieupt <phantrunghieu1412@gmail.com>
2025-03-14 12:15:19 +0700

    Update : Shared/Components

--- WorkFinder.Web/obj/Debug/net9.0/WorkFinder.Web.AssemblyInfo.cs
+++ WorkFinder.Web/obj/Debug/net9.0/WorkFinder.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkFinder.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6473686b239dd42c513ec5a2b2c065cb5fd13f42")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkFinder.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkFinder.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
